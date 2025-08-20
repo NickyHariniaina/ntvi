7e10a2ffd5e791500af99e22429dc5c55f81703f
NickyHariniaina <nickyharini1na@gmail.com>
2025-08-20 16:40:27 +0300

    fix: margin for printing existing text

--- src/editor/events/screen.rs
+++ src/editor/events/screen.rs
@@ -1,6 +1,9 @@
 use std::{error::Error, io::stdout};
 
-use crossterm::{cursor::MoveTo, execute, terminal};
+use crossterm::{
+    cursor::{MoveTo, MoveToColumn},
+    execute, terminal,
+};
 
 use crate::editor::{events::key::handle_key, ui::create_editor};
 
@@ -9,7 +12,7 @@ pub fn open_editor(buffer: String, has_file_attached: bool) -> Result<(), Box<dy
     create_editor()?;
     move_cursor_at_start()?;
     if has_file_attached {
-        print!("{}", buffer);
+        print_text(&buffer)?;
     }
     handle_key(buffer)?;
     terminal::disable_raw_mode()?;
@@ -19,5 +22,23 @@ pub fn open_editor(buffer: String, has_file_attached: bool) -> Result<(), Box<dy
 pub fn move_cursor_at_start() -> Result<(), Box<dyn Error>> {
     let mut stdout = stdout();
     execute!(stdout, MoveTo(0, 0))?;
+    Ok(())
+}
+
+pub fn print_text(buffer: &str) -> Result<(), Box<dyn Error>> {
+    for i in 0..buffer.len() {
+        match buffer.get(i..i + 1) {
+            Some("\n") => {
+                println!();
+                execute!(stdout(), MoveToColumn(0))?;
+            }
+            Some(char) => {
+                print!("{}", char);
+            }
+            None => {
+                break;
+            }
+        }
+    }
     Ok(())
 }
