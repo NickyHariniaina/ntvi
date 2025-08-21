6c28af4201fdb28127ba88600c70779d5e94f088
NickyHariniaina <nickyharini1na@gmail.com>
2025-08-21 09:53:22 +0300

    fix: cursor location track

--- src/editor/events/key.rs
+++ src/editor/events/key.rs
@@ -12,15 +12,22 @@ use std::{
 
 use crate::editor::events::file::save;
 
-pub fn handle_key(mut buffer: String) -> Result<(), Box<dyn Error>> {
+pub fn handle_key(mut buffer: String, cur_col: u16) -> Result<(), Box<dyn Error>> {
     'handle_key: loop {
         let event = event::read()?;
         if let Event::Key(key_event) = event {
+            let max_col = terminal::size()?.0;
+            let mut current_col: u16 = cur_col;
             match key_event.code {
                 KeyCode::Esc => {
                     break 'handle_key;
                 }
                 KeyCode::Char(key_pressed) => {
+                    if current_col == max_col {
+                        current_col = 0;
+                    } else {
+                        current_col += 1;
+                    }
                     print!("{}", key_pressed);
                     buffer.push(key_pressed);
                 }
@@ -37,6 +44,16 @@ pub fn handle_key(mut buffer: String) -> Result<(), Box<dyn Error>> {
                                 cursor::MoveToPreviousLine(1),
                                 cursor::MoveToColumn(last_lines_length as u16)
                             )?;
+                        } else if current_col == 0 {
+                            current_col = max_col;
+                            execute!(
+                                stdout(),
+                                cursor::MoveToPreviousLine(1),
+                                cursor::MoveToColumn(current_col)
+                            )?;
+                            print!("\x08");
+                            print!(" ");
+                            print!("\x08");
                         } else {
                             print!("\x08");
                             print!(" ");
--- src/editor/events/screen.rs
+++ src/editor/events/screen.rs
@@ -12,13 +12,13 @@ pub fn open_editor(buffer: String, has_file_attached: bool) -> Result<(), Box<dy
     terminal::enable_raw_mode()?;
     create_editor()?;
     move_cursor_at_start()?;
-
+    let mut current_col: u16 = 0;
     if has_file_attached {
-        let current_col = print_text(&buffer)?;
-        move_cursor_at_end_of_line(&current_col + 1)?;
+        current_col = print_text(&buffer)?;
+        move_cursor_at_end_of_line(current_col + 1)?;
     }
 
-    handle_key(buffer)?;
+    handle_key(buffer, current_col)?;
     terminal::disable_raw_mode()?;
     Ok(())
 }
