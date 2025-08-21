65c96aea52945c3bd609438737a02f015c030186
NickyHariniaina <nickyharini1na@gmail.com>
2025-08-21 09:40:54 +0300

    fix: better col tracking

--- src/editor/events/screen.rs
+++ src/editor/events/screen.rs
@@ -1,8 +1,9 @@
-use std::{error::Error, io::stdout};
+use std::{error::Error, io::stdout, thread::current};
 
 use crossterm::{
     cursor::{self, MoveTo, MoveToColumn, MoveToPreviousLine},
-    execute, terminal,
+    execute,
+    terminal::{self, DisableLineWrap, EnableLineWrap},
 };
 
 use crate::editor::{events::key::handle_key, ui::create_editor};
@@ -13,8 +14,8 @@ pub fn open_editor(buffer: String, has_file_attached: bool) -> Result<(), Box<dy
     move_cursor_at_start()?;
 
     if has_file_attached {
-        print_text(&buffer)?;
-        move_cursor_at_end_of_line(&buffer)?;
+        let current_col = print_text(&buffer)?;
+        move_cursor_at_end_of_line(current_col + 1)?;
     }
 
     handle_key(buffer)?;
@@ -28,20 +29,29 @@ pub fn move_cursor_at_start() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-pub fn move_cursor_at_end_of_line(buffer: &str) -> Result<(), Box<dyn Error>> {
-    let last_line_length = buffer.lines().last().map(|l| l.len()).unwrap_or(0);
-    execute!(stdout(), MoveToColumn(last_line_length as u16))?;
+pub fn move_cursor_at_end_of_line(current_col: u16) -> Result<(), Box<dyn Error>> {
+    execute!(stdout(), MoveToColumn(current_col))?;
     Ok(())
 }
 
-pub fn print_text(buffer: &str) -> Result<(), Box<dyn Error>> {
+pub fn print_text(buffer: &str) -> Result<u16, Box<dyn Error>> {
+    let window_size = terminal::size()?;
+    let max_col = window_size.0;
+    let mut current_col: u16 = 0;
     for i in 0..buffer.len() {
         match buffer.get(i..i + 1) {
             Some("\n") => {
                 println!();
+                current_col = 0;
                 execute!(stdout(), MoveToColumn(0))?;
             }
             Some(char) => {
+                if current_col == max_col {
+                    println!();
+                    current_col = 0;
+                } else {
+                    current_col += 1;
+                }
                 print!("{}", char);
             }
             None => {
@@ -49,5 +59,5 @@ pub fn print_text(buffer: &str) -> Result<(), Box<dyn Error>> {
             }
         }
     }
-    Ok(())
+    Ok(current_col)
 }
